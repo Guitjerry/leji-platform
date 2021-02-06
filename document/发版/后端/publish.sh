50a4d395e75be76c2193784571afd5aff9aa736a
jsd12060123@163.com <jerry>
2021-02-06 10:41:54 +0800

    发版更新

--- document/发版/后端/publish.sh
+++ document/发版/后端/publish.sh
@@ -5,6 +5,7 @@ mv /usr/local/leji-background/leji-platform-mall-background/mall-auth/target/mal
 mv /usr/local/leji-background/leji-platform-mall-background/mall-gateway/target/mall-gateway-1.0-SNAPSHOT.jar /usr/local/Programs
 mv /usr/local/leji-background/leji-platform-mall-background/mall-portal/target/mall-portal-1.0-SNAPSHOT.jar /usr/local/Programs
 mv /usr/local/leji-background/leji-platform-mall-background/mall-admin/target/mall-admin-1.0-SNAPSHOT.jar /usr/local/Programs
+cd /usr/local/Programs
 java -jar mall-auth-1.0-SNAPSHOT.jar &
 java -jar mall-gateway-1.0-SNAPSHOT.jar &
 java -jar mall-portal-1.0-SNAPSHOT.jar &
