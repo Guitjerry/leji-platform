e8e85609d8e64e5eea8bf166fa1f3fcaa35eff3c
jerry <jsd12060123@163.com>
2021-03-06 12:28:29 +0800

    更新

--- document/发版/后端/publish.sh
+++ document/发版/后端/publish.sh
@@ -2,11 +2,11 @@ cd /usr/local/leji-background/leji-platform
 git pull origin master
 mvn clean install -D skipTests
 cd /usr/local/leji-background/leji-platform/mall-auth/target
-nohup java -jar mall-auth-1.0-SNAPSHOT.jar
+nohup java -jar mall-auth-1.0-SNAPSHOT.jar &
 exit
 cd /usr/local/leji-background/leji-platform/mall-gateway/target
-nohup java -jar mall-gateway-1.0-SNAPSHOT.jar
+nohup java -jar mall-gateway-1.0-SNAPSHOT.jar &
 exit
 cd /usr/local/leji-background/leji-platform/mall-admin/target
-nohup java -jar mall-admin-1.0-SNAPSHOT.jar
+nohup java -jar mall-admin-1.0-SNAPSHOT.jar &
 exit
