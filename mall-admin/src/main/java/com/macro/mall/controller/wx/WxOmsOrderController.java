e33e729a0d47b13bd4f5f24df42076ead5f57966
jerry <jsd12060123@163.com>
2021-03-23 23:31:17 +0800

    更新分类选择

--- mall-admin/src/main/java/com/macro/mall/controller/wx/WxOmsOrderController.java
+++ mall-admin/src/main/java/com/macro/mall/controller/wx/WxOmsOrderController.java
@@ -1,15 +1,9 @@
 package com.macro.mall.controller.wx;
-
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import com.macro.mall.common.api.CommonPage;
 import com.macro.mall.common.api.CommonResult;
 import com.macro.mall.dto.*;
 import com.macro.mall.model.OmsOrder;
-import com.macro.mall.model.UmsMember;
-import com.macro.mall.service.CmsSubjectService;
 import com.macro.mall.service.OmsOrderService;
-import com.macro.mall.service.impl.OrderCombineManager;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 /**
  * 订单管理Controller Created by macro on 2018/10/11.
@@ -49,10 +40,9 @@ public class WxOmsOrderController {
   @RequestMapping(value = "/list", method = RequestMethod.GET)
   @ResponseBody
   public CommonResult<CommonPage<OmsOrder>> list(OmsOrderQueryParam queryParam,
-    @RequestParam(value = "pageSize", defaultValue = "100") Integer pageSize,
-    @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum) {
+                                                 @RequestParam(value = "pageSize", defaultValue = "100") Integer pageSize,
+                                                 @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum) {
     List<OmsOrder> orderList = orderService.list(queryParam, pageSize, pageNum);
     return CommonResult.success(CommonPage.restPage(orderList));
   }
-
 }
