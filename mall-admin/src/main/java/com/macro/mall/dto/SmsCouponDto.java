1c03d0d71a986ddd5bcc777518f97010522fbc84
jerry <jsd12060123@163.com>
2021-04-06 16:50:11 +0800

    更新优惠券

--- mall-admin/src/main/java/com/macro/mall/dto/SmsCouponDto.java
+++ mall-admin/src/main/java/com/macro/mall/dto/SmsCouponDto.java
@@ -1,6 +1,7 @@
 package com.macro.mall.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.macro.mall.model.SmsCoupon;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.Date;
@@ -11,4 +12,6 @@ public class SmsCouponDto extends SmsCoupon {
     private String productCategoryName;
     @JsonFormat(pattern="yyyy-MM-dd",timezone = "GMT+8")
     private Date endTime;
+    @ApiModelProperty(value = "即将过期 0否 1是")
+    private Integer nearExpire;
 }
