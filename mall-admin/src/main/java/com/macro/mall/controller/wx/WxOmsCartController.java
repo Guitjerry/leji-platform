9c878a66924bc01f6483e2a01a29ee79b6adf7fc
jerry <jsd12060123@163.com>
2021-04-08 19:01:54 +0800

    店铺数据中心

---
+++ mall-admin/src/main/java/com/macro/mall/controller/wx/WxOmsCartController.java
@@ -0,0 +1,15 @@
+package com.macro.mall.controller.wx;
+
+import io.swagger.annotations.Api;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ */
+@Controller
+@Api(tags = "WxOmsCartController", description = "购物车相关")
+@RequestMapping("/wxApi/cart")
+public class WxOmsCartController {
+
+}
